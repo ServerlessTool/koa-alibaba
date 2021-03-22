137cbd96c287605ba5bec245b6e387107a4dd705
hanxie <hanxie.wq@alibaba-inc.com>
2021-03-22 15:35:40 +0800

    add lib files

---
+++ lib/bootstrap.d.ts
@@ -0,0 +1,4 @@
+export declare const DEFAULTBOOTSTRAP = "#!/usr/bin/env bash\n\nexport DEFAULTAPP={app}\n{start}";
+export declare const DEFAULTAPP = "app.js";
+export declare const DEFAULTSTART = "node $DEFAULTAPP";
+//# sourceMappingURL=bootstrap.d.ts.map
